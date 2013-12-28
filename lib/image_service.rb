d6ae641cbfe41b4d3a29f2d27a8ec85aea9a9605
Jakob <jakob@jakobra.com>
2013-12-28 19:33:19 +0100

    Adjustment to resize_and crop ... no padding (stupid ide =))

--- lib/image_service.rb
+++ lib/image_service.rb
@@ -31,7 +31,7 @@ module Tantrum
     
     def self.resize_and_crop(width, height, image_content)
       image = MiniMagick::Image.read(image_content)
-      image.resize_and_pad(width, height, background = :transparent, gravity = 'Center')
+      image.resize "#{width}x#{height}^"
       if image[:width] > width
         x_start = (image[:width] - width) / 2
         image.crop("#{width}x#{height}+#{x_start}+0")
